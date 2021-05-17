55a13726cd4b3ce097fe7a851ffb52322d99c87a
TbabmBarry <liuchicheng0401@hotmail.com>
2021-05-17 13:54:15 +0200

    create trajectory and card views

---
+++ src/views/Index.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app class="deep-purple">
+        <Navbar />
+        <v-main class="grey lighten-4">
+            <router-view></router-view>
+        </v-main>
+    </v-app>
+</template>
+<script>
+import Navbar from "../components/Navbar";
+
+export default {
+    name: "index",
+    components: {
+        Navbar
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Trajectory.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="trajectory">Trajectory</div>
+</template>
+
+<script>
+export default {
+    name: "trajectory"
+}
+</script>
+
+<style>
+
+</style>
