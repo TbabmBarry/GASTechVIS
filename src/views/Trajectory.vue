7dd988b2cacc9711954d0718d381b389c766350d
TbabmBarry <liuchicheng0401@hotmail.com>
2021-05-17 20:42:58 +0200

    Add card and selectable actions

--- src/views/Trajectory.vue
+++ src/views/Trajectory.vue
@@ -1,10 +1,123 @@
 <template>
-  <div class="trajectory">Trajectory</div>
+    <v-app>
+        <v-container
+            fill-height
+        >
+            <v-col
+                cols="4"
+            >
+                <v-card>
+                    <v-list-item three-line>
+                        <v-list-item-content>
+                            <div class="overline mb-4">
+                            LEGEND
+                            </div>
+                            <v-list-item-title class="headline mb-1">
+                            Car ID
+                            </v-list-item-title>
+                            <v-list-item-subtitle>Select one specific car id to check its trajectory</v-list-item-subtitle>
+                        </v-list-item-content>
+
+                        <v-list-item-avatar
+                            tile
+                            size="80"
+                            color="grey"
+                        ></v-list-item-avatar>
+                        </v-list-item>
+                    <v-card-actions>
+                        <v-select
+                        :items="car_id"
+                        filled
+                        label="Select"
+                        ></v-select>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+            <v-col
+                cols="8"
+            >
+                <svg class="chart"></svg>
+            </v-col>
+        </v-container>
+  </v-app>
 </template>
 
 <script>
+import * as d3 from "d3";
+// import background from "../assets/MC2-tourist.jpg";
+
 export default {
-    name: "trajectory"
+    name: "trajectory",
+    data() {
+        return {
+            mapContainer: null,
+            abila: null,
+            car_paths: null,
+            car_id: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 101, 104, 105, 106, 107]
+        }
+    },
+    mounted() {
+        this.generateChart();
+    },
+    methods: {
+        generateChart() {
+            const svgWidth = 900;
+            const svgHeight = 600;
+            const padding = 30;
+
+            this.mapContainer = d3.select(".chart")
+                .attr("height", svgHeight)
+                .attr("width", svgWidth);
+
+            const x0 = padding;
+            const y0 = padding;
+            const x1 = svgWidth - padding * 2;
+            const y1 = svgHeight - padding * 2;
+
+            Promise.all([
+                d3.json("http://localhost:8000/Abila.geojson"),
+                d3.json("http://localhost:8000/gps_car_1_path_6.geojson")
+            ])
+            .then(([abila, car_paths]) => {
+                console.log(abila);
+                let projection = d3.geoMercator().fitExtent(
+                    [
+                        [x0, y0], //left upper coordinate
+                        [x1, y1], //right lower coordinate
+                    ], abila);
+                console.log(projection);
+
+                let pathGenerator = d3.geoPath().projection(projection);
+
+                // this.mapContainer.append("svg:image")
+                //     .attr("xlink:href", require('../assets/MC2-tourist.jpg'))
+                //     .attr("width", svgWidth)
+                //     .attr("height", svgHeight)
+                //     .attr("x", 0)
+                //     .attr("y", 0);
+                this.mapContainer.selectAll("path")
+                    .data(abila.features) //data binding
+                    .join("path")
+                    .attr("d", pathGenerator) //draw path
+                    .attr("stroke-width", 0.5)
+                    .attr("stroke", "#000000")
+                    .attr("fill", "#ffffff");
+                this.mapContainer.append("path")
+                    .attr("d", pathGenerator(car_paths))
+                    .style("fill", "none")
+                    .style("stroke", "orange");
+                
+            })
+
+            const zoom = d3.zoom()
+                .on('zoom', (event) => {
+                    this.mapContainer.attr('transform', event.transform);
+                })
+                .scaleExtent([1, 40]);
+            this.mapContainer.call(zoom).append('g');
+
+        }
+    }
 }
 </script>
 
