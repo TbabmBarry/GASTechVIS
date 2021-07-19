d8a4f8c4f06e6b38e47e6aceeb627635dbc93513
Chicheng Liu <liuchicheng0401@hotmail.com>
2021-07-19 22:38:19 +0200

    update: project upgrade to Vue 3

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import Vue from "vue";
-import ECharts from 'vue-echarts';
+import Vue, { createApp } from "vue";
+import ECharts from "vue-echarts";
 import App from "./App.vue";
-import router from './router';
+import router from "./router";
 import vuetify from "./plugins/vuetify";
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import locale from 'element-ui/lib/locale/lang/en'
-
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import locale from "element-ui/lib/locale/lang/en";
 
 import { use } from "echarts/core";
 import { CanvasRenderer } from "echarts/renderers";
@@ -16,22 +15,23 @@ import {
   TooltipComponent,
   LegendComponent,
   VisualMapComponent,
-} from 'echarts/components';
+} from "echarts/components";
 use([
   CanvasRenderer,
   HeatmapChart,
   GridComponent,
   TooltipComponent,
   LegendComponent,
-  VisualMapComponent
+  VisualMapComponent,
 ]);
 Vue.use(ElementUI, { locale });
-Vue.component('v-chart', ECharts);
-Vue.config.productionTip = false;
-new Vue({
-  router,
+Vue.component("v-chart", ECharts);
+createApp({
   vuetify,
+
   render: function (h) {
     return h(App);
   },
-}).$mount("#app");
+})
+  .use(router)
+  .mount("#app");
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,22 +1,20 @@
 <template>
-    <v-app class="deep-purple">
-        <Navbar />
-        <v-main class="grey lighten-4">
-            <router-view></router-view>
-        </v-main>
-    </v-app>
+  <v-app class="deep-purple">
+    <Navbar />
+    <v-main class="grey lighten-4">
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 <script>
 import Navbar from "../components/Navbar";
 
 export default {
-    name: "index",
-    components: {
-        Navbar
-    }
-}
+  name: "index",
+  components: {
+    Navbar,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
